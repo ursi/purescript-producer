3e4ea12c6661f0419dd2561a33fcd1e518da3740
Mason Mackaman <masondeanm@aol.com>
2021-05-22 01:30:48 -0400

    Add producerN functions

--- src/Producer.purs
+++ src/Producer.purs
@@ -3,6 +3,11 @@ module Producer
   , class Producible
   , produce
   , producer
+  , producer2
+  , producer3
+  , producer4
+  , producer5
+  , producer6
   , lift
   , liftRefEq
   )
@@ -44,6 +49,52 @@ instance eqProducer :: Eq (Producer a) where
 
 producer :: ∀ a b. Eq b => (b -> a) -> b -> Producer a
 producer f b = Producer (_ $ f /\ b)
+
+producer2 :: ∀ a b c. Eq (b /\ c) => (b -> c -> a) -> b -> c -> Producer a
+producer2 f b c = producer producer2Helper $ RefEq f /\ b /\ c
+
+producer2Helper :: ∀ a b c. RefEq (b -> c -> a) /\ b /\ c -> a
+producer2Helper (RefEq f /\ b /\ c) = f b c
+
+producer3 :: ∀ a b c d. Eq (b /\ c /\ d) => (b -> c -> d -> a) -> b -> c -> d -> Producer a
+producer3 f b c d = producer producer3Helper $ RefEq f /\ b /\ c /\ d
+
+producer3Helper :: ∀ a b c d. RefEq (b -> c -> d -> a) /\ b /\ c /\ d -> a
+producer3Helper (RefEq f /\ b /\ c /\ d) = f b c d
+
+producer4 :: ∀ a b c d e.
+  Eq (b /\ c /\ d /\ e)
+  => (b -> c -> d -> e -> a)
+  -> b -> c -> d -> e
+  -> Producer a
+producer4 f b c d e = producer producer4Helper $ RefEq f /\ b /\ c /\ d /\ e
+
+producer4Helper :: ∀ a b c d e. RefEq (b -> c -> d -> e -> a) /\ b /\ c /\ d /\ e -> a
+producer4Helper (RefEq f /\ b /\ c /\ d /\ e) = f b c d e
+
+producer5 :: ∀ a b c d e f.
+  Eq (b /\ c /\ d /\ e /\ f)
+  => (b -> c -> d -> e -> f -> a)
+  -> b -> c -> d -> e -> f
+  -> Producer a
+producer5 fn b c d e f = producer producer5Helper $ RefEq fn /\ b /\ c /\ d /\ e /\ f
+
+producer5Helper :: ∀ a b c d e f.
+  RefEq (b -> c -> d -> e -> f -> a) /\ b /\ c /\ d /\ e /\ f -> a
+producer5Helper (RefEq fn /\ b /\ c /\ d /\ e /\ f) = fn b c d e f
+
+producer6 :: ∀ a b c d e f g.
+  Eq (b /\ c /\ d /\ e /\ f /\ g)
+  => (b -> c -> d -> e -> f -> g -> a)
+  -> b -> c -> d -> e -> f -> g
+  -> Producer a
+producer6 fn b c d e f g =
+  producer producer6Helper
+  $ RefEq fn /\ b /\ c /\ d /\ e /\ f /\ g
+
+producer6Helper :: ∀ a b c d e f g.
+  RefEq (b -> c -> d -> e -> f -> g -> a) /\ b /\ c /\ d /\ e /\ f /\ g -> a
+producer6Helper (RefEq fn /\ b /\ c /\ d /\ e /\ f /\ g) = fn b c d e f g
 
 lift :: ∀ a. Eq a => a -> Producer a
 lift = producer identity
--- test/Main.purs
+++ test/Main.purs
@@ -3,11 +3,33 @@ module Test.Main where
 import MasonPrelude hiding (log)
 
 import Effect.Console (log)
-import Producer (producer, produce, lift, liftRefEq)
+import Producer
+  ( producer
+  , producer2
+  , producer3
+  , producer4
+  , producer5
+  , producer6
+  , produce
+  , lift
+  , liftRefEq
+  )
 import Test.Assert (assert', assertFalse')
 
 plus1 :: Int -> Int
 plus1 = (_ + 1)
+
+add3 :: Int -> Int -> Int -> Int
+add3 a b c = a + b + c
+
+add4 :: Int -> Int -> Int -> Int -> Int
+add4 a b c d = a + b + c + d
+
+add5 :: Int -> Int -> Int -> Int -> Int -> Int
+add5 a b c d e = a + b + c + d + e
+
+add6 :: Int -> Int -> Int -> Int -> Int -> Int -> Int
+add6 a b c d e f = a + b + c + d + e + f
 
 main :: Effect Unit
 main = do
@@ -17,3 +39,13 @@ main = do
   assert' "4" $ (produce $ plus1 <$> lift 5) == 6
   assert' "5" $ liftRefEq plus1 == liftRefEq plus1
   assertFalse' "6" $ liftRefEq plus1 == liftRefEq (_ + 1)
+  assert' "7" $ producer2 (+) 1 2 == producer2 (+) 1 2
+  assertFalse' "8" $ producer2 (+) 1 2 == producer2 (+) 2 2
+  assert' "9" $ producer3 add3 1 2 3 == producer3 add3 1 2 3
+  assertFalse' "10" $ producer3 add3 1 2 3 == producer3 add3 2 2 3
+  assert' "11" $ producer4 add4 1 2 3 4 == producer4 add4 1 2 3 4
+  assertFalse' "12" $ producer4 add4 1 2 3 4 == producer4 add4 2 2 3 4
+  assert' "13" $ producer5 add5 1 2 3 4 5 == producer5 add5 1 2 3 4 5
+  assertFalse' "14" $ producer5 add5 1 2 3 4 5 == producer5 add5 2 2 3 4 5
+  assert' "15" $ producer6 add6 1 2 3 4 5 6 == producer6 add6 1 2 3 4 5 6
+  assertFalse' "16" $ producer6 add6 1 2 3 4 5 6 == producer6 add6 2 2 3 4 5 6
